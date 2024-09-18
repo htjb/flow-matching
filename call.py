d1ced0c0fd636407d633db57ef52e6c4c39fa802
htjb <harrytjbevins@yahoo.co.uk>
2024-09-18 16:04:01 +0100

    making nice gifs

--- call.py
+++ call.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dist = mixture_normal([0,0], [[0,0],[0,0]], 
-                      np.array([[[1, 0.99],[0.99, 1]],[[1, -0.99],[-0.99, 1]]]))
+                      np.array([[[1, 0.99],[0.99, 1]],
+                                [[1, -0.99],[-0.99, 1]]]))
 samples = dist.rvs(10000)
 
 """plt.scatter(samples[:,0], samples[:,1])
@@ -13,7 +14,7 @@ plt.show()"""
 fm = FlowMatch(samples, 2, 3, 100)
 fm.train(epochs=100)
 
-samps = fm.sample(1000)
+samps = fm.sample(1000, time_samples=100)
 plt.scatter(samps[-1, :, 0], samps[-1, :, 1])
 plt.show()
 
@@ -22,6 +23,12 @@ lps = fm.log_prob(samples)
 print(samps.shape)
 
 for i in range(samps.shape[0]):
-    if i%1 == 0:
-        plt.plot(samps[i, :, 0], samps[i, :, 1], '.', c='k', alpha=0.2)
-    plt.show()
+    plt.plot(samps[i, :, 0], samps[i, :, 1], '.', c='k', alpha=0.2)
+    plt.xlim(samps[:, :, 0].min(), samps[:, :, 0].max())
+    plt.ylim(samps[:, :, 1].min(), samps[:, :, 1].max())
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig('example/flowmatch_{:03d}.png'.format(i))
+    plt.close()
